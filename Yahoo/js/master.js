e35b5c661d38446a299439710e5691e7fd9ada3d
Sudip Purkayastha <sudip_purkayastha@ahm.honda.com>
2019-01-14 20:17:27 -0800

    Initial commit

---
+++ Yahoo/js/master.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+document.write('<script type="text/javascript" src="yahoo/js/yahoo.js"><\/script>');
+document.write('<script type="text/javascript" src="yahoo/js/event.js"><\/script>');
+document.write('<script type="text/javascript" src="yahoo/js/dom.js"><\/script>');
+document.write('<script type="text/javascript" src="yahoo/js/dragdrop.js"><\/script>');
+document.write('<script type="text/javascript" src="yahoo/js/container.js"><\/script>');
+document.write('<script type="text/javascript" src="yahoo/js/menu.js"><\/script>');
+document.write('<script type="text/javascript" src="yahoo/js/animation.js"><\/script>');
+document.write('<script type="text/javascript" src="yahoo/js/calendar.js"><\/script>');
+document.write('<script type="text/javascript" src="yahoo/js/treeview.js"><\/script>');
+document.write('<script type="text/javascript" src="yahoo/js/TaskNode.js"><\/script>');
